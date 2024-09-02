937aae9e04c80da55dd4de12e57acdaf841de758
EveningComet <165712308+EveningComet@users.noreply.github.com>
2024-09-02 13:30:38 -0400

    Experience UI Code
    Implemented some code for handling experience points in the UI.

--- Scripts/UI/General/PartyMemberSlot.gd
+++ Scripts/UI/General/PartyMemberSlot.gd
@@ -9,6 +9,9 @@ class_name PartyMemberSlot extends CombatantHUD
 @export var sp_bar: Vitalbar
 @export var xp_bar: ProgressBar
 
+## How long, in seconds, it takes for the xp bar to increase.
+@export var xp_process_time: float = 1.0
+
 @export var char_name_label: Label
 
 func set_combatant(pc: PlayerCombatant) -> void:
@@ -18,6 +21,7 @@ func set_combatant(pc: PlayerCombatant) -> void:
 	if combatant != null:
 		combatant.stat_changed.connect( on_stat_changed )
 		char_name_label.set_text(pc.char_name)
+		combatant.experience_gained.connect( on_experience_gained )
 		on_stat_changed(combatant)
 		
 		# Enable the things the player should see
@@ -33,3 +37,21 @@ func on_stat_changed(pc: Combatant) -> void:
 	
 	hp_bar.update_display(curr_health, max_health)
 	sp_bar.update_display(curr_sp, max_sp)
+
+## When the monitored player character gains experience points, update to show
+## the visual change.
+func on_experience_gained(growth_data: Array) -> void:
+	for line in growth_data:
+		var desired_experience = line[0]
+		var max_experience     = line[1]
+		xp_bar.max_value       = max_experience
+		await animate_xp_bar(desired_experience, xp_process_time)
+		if xp_bar.max_value == xp_bar.value:
+			# TODO: If an animation/effect/etc. is dresired, play it here.
+			xp_bar.value = xp_bar.min_value
+
+func animate_xp_bar(target, duration: float = 1.0) -> void:
+	var tween = create_tween()
+	tween.tween_property(xp_bar, "value", target,
+	duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	await tween.finished
