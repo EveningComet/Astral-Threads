c051de82ea72b4e7925bb03f18ebacb12e840791
thannicko <thannicko@home>
2025-02-12 21:55:45 +0100

    Compile the old skill menu

---
+++ Scripts/UI/Skill Menu/AttributesMenu.gd
@@ -0,0 +1,63 @@
+class_name AttributesMenu extends Node
+
+signal attribute_points_depleted
+signal attribute_points_available
+
+@export var points_label: Label
+@export var container: VBoxContainer
+@export var menu_entry_template: PackedScene
+@export var upgrader: AttributesUpgrader
+@export var confirm_button: Button
+@export var undo_button: Button
+
+var character: PlayerCombatant
+var draft_points: int = 0
+
+func initialize(_character_changed: Signal):
+	_character_changed.connect( update_and_render )
+	upgrader.stats_confirmed.connect( confirm )
+	upgrader.stats_undone.connect( undo )
+	upgrader.attribute_upgraded.connect( attribute_upgraded )
+	spawn_entries_for_attributes()
+
+func update_and_render(_character: PlayerCombatant):
+	character = _character
+	set_draft_points( character.available_attribute_points )
+
+func confirm():
+	character.available_attribute_points = draft_points
+	set_draft_points( character.available_attribute_points )
+
+func undo():
+	set_draft_points( character.available_attribute_points )
+
+func attribute_upgraded():
+	set_draft_points( draft_points - 1 )
+
+func set_draft_points(points: int):
+	draft_points = points
+	emit_correct_signal()
+	update_points_label()
+	disable_confirm_and_undo_if_no_action_taken()
+
+func emit_correct_signal():
+	if draft_points == 0:
+		emit_signal( "attribute_points_depleted" )
+	elif draft_points > 0:
+		emit_signal( "attribute_points_available" )
+
+func update_points_label():
+	points_label.text = "Available attribute points: "
+	points_label.text += str( draft_points )
+
+func disable_confirm_and_undo_if_no_action_taken():
+	var no_action_taken := draft_points == character.available_attribute_points
+	confirm_button.disabled = no_action_taken
+	undo_button.disabled = no_action_taken
+
+func spawn_entries_for_attributes():
+	for attribute in StatHelper.attributes:
+		var entry := menu_entry_template.instantiate() as AttributeMenuEntry
+		entry.initialize(
+			attribute, upgrader, attribute_points_depleted, attribute_points_available )
+		container.add_child( entry )
---
+++ Scripts/UI/Skill Menu/AttributesUpgrader.gd
@@ -0,0 +1,90 @@
+## Interface for spending attribute points and skill points
+class_name AttributesUpgrader extends Node
+
+signal attribute_upgraded
+signal class_upgraded
+
+signal stats_confirmed
+signal stats_undone
+signal draft_stats_changed(new_stats: Dictionary)
+
+var character: PlayerCombatant
+var current_class: Job
+
+## Stores the classes and their potentially new levels.
+var classes_and_class_levels: Dictionary
+
+## Stores what the player's attributes will be depending on the upgrades.
+var draft_stats: Dictionary = {}
+var attributes := StatHelper.attributes
+
+func initialize(_character_changed: Signal, _class_changed: Signal):
+	_class_changed.connect( set_class )
+	_character_changed.connect( store_stats )
+
+func set_class(_new_class: Job):
+	current_class = _new_class
+	reset_draft_stats()
+
+func store_stats(_character: PlayerCombatant):
+	if character != null:
+		character.stat_changed.disconnect( on_character_stats_changed )
+	character = _character
+	character.stat_changed.connect( on_character_stats_changed )
+	classes_and_class_levels = character.pc_classes.duplicate()
+	reset_draft_stats()
+
+## When the stats of the monitored character change.
+func on_character_stats_changed(_character: PlayerCombatant) -> void:
+	draft_stats_changed.emit( draft_stats )
+
+func confirm():
+	# Upgrade the class level
+	for key: Job in classes_and_class_levels:
+		var diff: int = classes_and_class_levels[key] - character.pc_classes[key]
+		if diff > 0:
+			character.upgrade_class_by(key, diff)
+	
+	# Upgrade the attributes
+	for attribute in attributes:
+		var draft_val:    int = draft_stats[attribute]
+		var original_val: int = character.stats.stats[attribute as StatHelper.StatTypes].get_base_value()
+		var diff:         int = draft_val - original_val
+		if diff > 0:
+			character.raise_base_value_by(attribute, diff)
+	
+	reset_draft_stats()
+	stats_confirmed.emit()
+
+func undo():
+	reset_draft_stats()
+	stats_undone.emit()
+
+func attribute_upgrade(attribute: StatHelper.StatTypes):
+	draft_stats[attribute] += 1
+	attribute_upgraded.emit()
+	draft_stats_changed.emit(draft_stats)
+
+func class_upgrade(upgrading_class: Job):
+	classes_and_class_levels[upgrading_class] += 1
+	for attribute in attributes:
+		draft_stats[attribute] += upgrading_class.get_upgrade_attributes()[attribute]
+	
+	# The class is being upgraded, see if there are any skills that can be
+	# unlocked for that class
+	character.skill_holder.try_to_unlock_with_class_level(
+		current_class, classes_and_class_levels[upgrading_class]
+	)
+	
+	class_upgraded.emit()
+	draft_stats_changed.emit( draft_stats )
+
+func reset_draft_stats():
+	if current_class != null and character != null and character.pc_classes.has(current_class):
+		classes_and_class_levels[current_class] = character.pc_classes[current_class]
+		for attribute in attributes:
+			draft_stats[attribute] = character.stats.stats[attribute].get_base_value()
+		
+		# TODO: See if some skills have to be relocked.
+		
+		draft_stats_changed.emit( draft_stats )
